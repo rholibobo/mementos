fc0aad95efb6a916bcce97761cde8dcceff6ee39
rholibobo <rholipop@gmail.com>
2023-04-26 10:53:34 +0100

    Created post message schema and model

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+export const getPosts = (req, res) => {
+    res.send("This works")
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import {getPosts} from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get('/', )
+router.get('/', getPosts)
 
 export default router;
