4e0b6e0686ddfb3cea474b604ebe6c728865d3d6
rholibobo <rholipop@gmail.com>
2023-04-26 16:15:24 +0100

    Added form and post files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
 
 function App() {
-  return (
-    <div >
-      Hello World
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
