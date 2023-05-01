6758a23fb5697f9ffb9d7faa49ca67c8768e2de5
rholibobo <rholipop@gmail.com>
2023-05-01 17:57:38 +0100

    Implemented the delete functionality

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -7,44 +7,66 @@ import {
   Typography,
 } from "@mui/material";
 import { ThumbUpAlt, Delete, MoreHoriz } from "@mui/icons-material";
-import moment from "moment"
+import moment from "moment";
 import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import { deletePost } from "../../../actions/posts";
 
-const Post = ({post, setCurrentId}) => {
+const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
   const { classes } = useStyles();
   return (
     <Card className={classes.card}>
-        <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
-        <div className={classes.overlay}>
-            <Typography variant="h6">{post.creator}</Typography>
-            <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-        </div>
-        <div className={classes.overlay2}>
-            <Button style={{color: "white"}} size="small" onClick={() => setCurrentId(post._id)} >
-                <MoreHoriz fontSize='default' />
-            </Button>
-        </div>
-        <div className={classes.details}>
-        <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=> `#${tag} `)}</Typography>
-        </div>
-        <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
-        <CardContent>
-            <Typography variant="h5" gutterBottom>{post.message}</Typography>
-        </CardContent>
-        <CardActions className={classes.cardActions}>
-            <Button size="small" color="primary" onClick={()=>{}}>
-                <ThumbUpAlt fontSize='small'/>
-                Like
-                {post.likeCount}
-            </Button>
-            <Button size="small" color="primary" onClick={()=>{}}>
-                <Delete fontSize='small'/>
-                Delete
-            </Button>
-        </CardActions>
-        
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button
+          style={{ color: "white" }}
+          size="small"
+          onClick={() => setCurrentId(post._id)}
+        >
+          <MoreHoriz fontSize="default" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAlt fontSize="small" />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deletePost(post._id))}
+        >
+          <Delete fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
     </Card>
-  )
+  );
 };
 
 export default Post;
