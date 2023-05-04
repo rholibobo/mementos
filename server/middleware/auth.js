19b1fa95601a37c1fb6e909b38658151dd896771
rholibobo <rholipop@gmail.com>
2023-05-04 17:43:33 +0100

    Created middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+
+        // const for checking if it's our token. If length is less than 500, it is our own
+        const isCustomAuth = token.length < 500;
+
+        // Data gotten from token
+        let decodedData;
+
+        if(token && isCustomAuth) {
+            // We re gonna get the data of each token, i.e each username and Id. "test" is secretId for jwt used in the signin controller
+            decodedData = jwt.verify(token, "test");
+
+            // Store user Id
+            req.userId = decodedData?.id
+        } else {
+            // If working with Google's OAuth token...
+            decodedData = jwt.decode(token)
+
+            // Store user Id. "sub" is google's name for a specific id that diffrentiates every single google user
+            req.userId = decodedData?.sub;
+        }
+        
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default auth;
